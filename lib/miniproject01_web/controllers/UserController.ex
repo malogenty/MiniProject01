59598722aeaa1b732449575ee38e2699e586a273
itsmehujo <itsmehujo@gmail.com>
2022-10-24 15:40:26 +0200

    added base methods

--- lib/miniproject01_web/controllers/UserController.ex
+++ lib/miniproject01_web/controllers/UserController.ex
@@ -1,19 +1,19 @@
 defmodule Miniproject01Web.UserController do
   use Miniproject01Web, :controller
 
-  def index(conn, _params) do
+  def index(conn, _params, %{"userId" => id}) do
     json(conn, %{hello: "index"})
   end
 
-  def create(conn, _params) do
+  def create(conn) do
     json(conn, %{hello: "create"})
   end
 
-  def update(conn, _params) do
+  def update(conn, %{"userId" => id}) do
     json(conn, %{hello: "update"})
   end
 
-  def delete(conn, _params) do
+  def delete(conn, %{"userId" => id}) do
     json(conn, %{hello: "delete"})
   end
 end
