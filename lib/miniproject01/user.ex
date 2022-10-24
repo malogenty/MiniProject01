6a02da1b9ae5969ffd84c8d5983752040029b071
itsmehujo <itsmehujo@gmail.com>
2022-10-24 16:46:01 +0200

    read & read credentials

--- lib/miniproject01/user.ex
+++ lib/miniproject01/user.ex
@@ -2,6 +2,8 @@ defmodule ApiProject.User do
   use Ecto.Schema
   import Ecto.Changeset
   import EctoCommons.EmailValidator
+  import Ecto.Query, warn: false
+  alias ApiProject.Repo
 
   schema "users" do
     field(:email, :string)
@@ -16,5 +18,28 @@ defmodule ApiProject.User do
     |> cast(attrs, [:username, :email])
     |> validate_email(:email)
     |> validate_required([:username, :email])
+  end
+
+  def get_user!(id) do Repo.get!(Users, id)
+
+  def get_user_by_credentials!(email, username) do
+    query = from u in "users", where: u.email = email AND u.username = username, select: *
+    Repo.all(query)
+  end
+
+  def create_user(attrs \\ %{}) do
+      %Users{}
+      |> Users.changeset(attrs)
+      |> Repo.insert()
+  end
+
+  def update_users(%Users{} = users, attrs) do
+    users
+    |> Users.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_users(%Users{} = users) do
+    Repo.delete(users)
   end
 end
--- lib/miniproject01_web/controllers/UserController.ex
+++ lib/miniproject01_web/controllers/UserController.ex
@@ -6,8 +6,16 @@ defmodule ApiProjectWeb.UsersController do
 
   action_fallback(ApiProjectWeb.FallbackController)
 
+  def read(conn, _params, %{"userId" => id}) do
+    user = User.get_user!(id)
+  end
+
+  def readCredentials(conn, %{"email" => email, "username" => username}) do
+    user = User.get_user_by_credentials(%{email, username})
+  end
+
   def create(conn, %{"user" => user_params}) do
-    with {:ok, %Users{} = user} <- MiniProject01.create_user(user_params) do
+    with {:ok, %Users{} = user} <- User.create_user(user_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", Routes.users_path(conn, :show, user))
@@ -16,7 +24,7 @@ defmodule ApiProjectWeb.UsersController do
   end
 
   def update(conn, %{"userId" => id, "user" => user_params}) do
-    user = MiniProject01.get_user!(id)
+    user = User.get_user!(id)
 
     with {:ok, %Users{} = users} <- MiniProject01.update_user(user, user_params) do
       render(conn, "show.json", user: user)
