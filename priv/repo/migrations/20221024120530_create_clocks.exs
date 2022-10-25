c33cbce973be16299fab9b9e6c9e1f75f3eec628
itsmehujo <itsmehujo@gmail.com>
2022-10-25 15:46:18 +0200

    uniformised the user/user_id nomenclature

--- priv/repo/migrations/20221024120530_create_clocks.exs
+++ priv/repo/migrations/20221024120530_create_clocks.exs
@@ -5,11 +5,11 @@ defmodule ApiProject.Repo.Migrations.CreateClocks do
     create table(:clocks) do
       add(:time, :naive_datetime, null: false)
       add(:status, :boolean, default: true, null: false)
-      add(:user, references(:users, on_delete: :nothing), null: false)
+      add(:user_id, references(:users, on_delete: :nothing), null: false)
 
       timestamps()
     end
 
-    create(index(:clocks, [:user]))
+    create(index(:clocks, [:user_id]))
   end
 end
