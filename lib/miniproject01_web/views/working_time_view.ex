4ac1606456503a481f5d15b4818e8fcb588fd4e1
Enzo Rojo <enzo.rojo-allendes@epitech.eu>
2022-10-25 15:08:54 +0200

    update json return

--- lib/miniproject01_web/views/working_time_view.ex
+++ lib/miniproject01_web/views/working_time_view.ex
@@ -3,14 +3,17 @@ defmodule ApiProjectWeb.WorkingTimeView do
   alias ApiProjectWeb.WorkingTimeView
 
   def render("index.json", %{working_times: working_times}) do
-    %{data: render_many(working_times, WorkingTimeView, "working_time.json")}
+    for working_time <- working_times do
+      %{
+        id: working_time.id,
+        start: working_time.start,
+        end: working_time.end,
+        user: working_time.user_id
+      }
+    end
   end
 
   def render("show.json", %{working_time: working_time}) do
-    %{data: render_one(working_time, WorkingTimeView, "working_time.json")}
-  end
-
-  def render("working_time.json", %{working_time: working_time}) do
     %{
       id: working_time.id,
       start: working_time.start,
