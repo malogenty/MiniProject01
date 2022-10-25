d48b909ac7c0fce4ecb53f6c61a71629ad31db50
a <a@example.org>
2022-10-25 12:07:13 +0200

    Adding base for ClockController

--- lib/miniproject01/clock.ex
+++ lib/miniproject01/clock.ex
@@ -1,6 +1,9 @@
 defmodule ApiProject.Clock do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query, warn: false
+  alias ApiProject.Repo
+  alias ApiProject.Clock
 
   schema "clocks" do
     field(:status, :boolean, default: true)
@@ -15,5 +18,15 @@ defmodule ApiProject.Clock do
     clock
     |> cast(attrs, [:time, :status])
     |> validate_required([:time, :status])
+  end
+
+  def create(attrs \\ %{}) do
+    %Clock{}
+    |> Clock.changeset(attrs)
+    |> Repo.insert()
+    |> case do
+      {:ok, clock} -> {:ok, clock}
+      {:error, _} -> :error
+    end
   end
 end
--- lib/miniproject01_web/controllers/ClockController.ex
+++ lib/miniproject01_web/controllers/ClockController.ex
@@ -1,6 +1,18 @@
 defmodule ApiProjectWeb.ClockController do
   use ApiProjectWeb, :controller
+  alias ApiProject.Clock
+  
   def read(conn, _params) do
     json(conn, %{id: "qsd"})
-  end    
+  end
+  def create(conn, %{"clock" => clock_params}) do
+    with {:ok, %Clock{} = clock} <- Clock.create(clock_params) do
+      render(conn, "clock.json", clock: clock)
+    else
+      :error ->
+        conn
+        |> put_status(400)
+        |> render("error.json", reason: "The payload does not match the expected pattern")
+    end
+  end
 end
