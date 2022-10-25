0f1c6e739f679862f87335b17af2651ee24c8f03
Enzo Rojo <enzo.rojo-allendes@epitech.eu>
2022-10-25 12:09:41 +0200

    generated working time controller

---
+++ lib/miniproject01_web/controllers/working_time_controller.ex
@@ -0,0 +1,42 @@
+defmodule ApiProjectWeb.WorkingTimeController do
+  use ApiProjectWeb, :controller
+
+  alias ApiProject.WorkingTime
+
+  action_fallback ApiProjectWeb.FallbackController
+
+  def readAll(conn, _params) do
+    working_times = WorkingTime.list_working_times()
+    render(conn, "index.json", working_times: working_times)
+  end
+
+  def readOne(conn, %{"id" => id}) do
+    working_time = WorkingTime.get_working_time!(id)
+    render(conn, "show.json", working_time: working_time)
+  end
+
+  def create(conn, %{"working_time" => working_time_params}) do
+    with {:ok, %WorkingTime{} = working_time} <- WorkingTime.create_working_time(working_time_params) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", Routes.working_time_path(conn, :show, working_time))
+      |> render("show.json", working_time: working_time)
+    end
+  end
+
+  def update(conn, %{"id" => id, "working_time" => working_time_params}) do
+    working_time = WorkingTime.get_working_time!(id)
+
+    with {:ok, %WorkingTime{} = working_time} <- WorkingTime.update_working_time(working_time, working_time_params) do
+      render(conn, "show.json", working_time: working_time)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    working_time = WorkingTime.get_working_time!(id)
+
+    with {:ok, %WorkingTime{}} <- WorkingTime.delete_working_time(working_time) do
+      send_resp(conn, :no_content, "")
+    end
+  end
+end
