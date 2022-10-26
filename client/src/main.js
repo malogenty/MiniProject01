2241521965245fcb2e3adabdd651c4c7cf3e26fa
a <a@example.org>
2022-10-26 11:12:59 +0200

    Adding router to code

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const Test = { template: '<div>Test</div>' }
+
+const routes = [
+  { path: '/test', component: Test },
+]
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
