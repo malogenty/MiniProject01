1a58b8e15ce0c2ec3eb6359cdcc841f4e332dfe7
a <a@example.org>
2022-10-26 11:58:05 +0200

    Adding router

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,35 +1,30 @@
-<script>
-import ClockComponent from './components/ClockComponent.vue'
-import NotFound from './components/NotFound.vue'
-import BaseFile from './components/BaseFile.vue'
-
-const routes = {
-  '/': BaseFile,
-  '/about': ClockComponent
-}
-
-export default {
-  data() {
-    return {
-      currentPath: window.location.hash
-    }
-  },
-  computed: {
-    currentView() {
-      return routes[this.currentPath.slice(1) || '/'] || NotFound
-    }
-  },
-  mounted() {
-    window.addEventListener('hashchange', () => {
-      this.currentPath = window.location.hash
-		})
-  }
-}
-</script>
-
 <template>
-  <a href="#/">Home</a> |
-  <a href="#/about">About</a> |
-  <a href="#/non-existent-path">Broken Link</a>
-  <component :is="currentView" />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">-->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BaseFile from '@/components/BaseFile.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    BaseFile
+  }
+}
+</script>
