6235e14a6a1ab7ebcfe35da9763a38de067d5721
Enzo Rojo <enzo.rojo-allendes@epitech.eu>
2022-10-27 11:00:45 +0200

    added chart view

---
+++ client/src/views/ChartView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="chart">
+    <h1>Chart</h1>
+    <BarChart :data="chartData"/>
+  </div>
+</template>
+
+<script>
+import BarChart from '../components/BarChart.vue'
+import fakeWorkingTimes from '../dummies/fakeWorkingTimes.json'
+import { getDayDurationsByWeeks } from '../utils/workingtimes_utils.js'
+export default {
+  name: 'HomeView',
+  components: {
+    BarChart
+  },
+  data() {
+    return {
+      chartData: getDayDurationsByWeeks(fakeWorkingTimes)['43']
+    }
+  }
+
+}
+</script>
