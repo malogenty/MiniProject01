c6e0b9559df6f8338e8cff6730467cc928cb10df
itsmehujo <itsmehujo@gmail.com>
2022-10-27 17:13:01 +0200

    added some layout

---
+++ client/src/components/Dashboard/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="controls">
+      <div class="back">
+        <img src="/icons/arrow-left.png" alt="">
+      </div>
+      <span class="user">{{ user.username }}</span>
+      <div class="full-screen" @click="$emit('toggleFullScreen')">
+        <img src="/icons/fullscreen.png" alt="" v-if="fullscreen">
+        <img src="/icons/fullscreen-exit.png" alt="" v-else>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    fullscreen: Boolean
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.controls {
+    padding: 1vw;
+    display: flex;
+    justify-content: space-between;
+    div.back,
+    div.full-screen {
+      cursor: pointer;
+      img {
+        width: 36px;
+        height: 36px;
+      }
+    }
+  }
+</style>
