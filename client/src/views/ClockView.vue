5602a489c17dc609cc2db2ee6768f04187207e49
a <a@example.org>
2022-10-27 12:35:23 +0200

    Adding basic clock button

--- client/src/views/ClockView.vue
+++ client/src/views/ClockView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="about">
     <h1>Clock page for: {{this.$route.params.username}}</h1>
-    <button @click="qsd">Clock In</button>
+    <h1>{{ user.clocks }}</h1>
+    <button @click="clocked = !clocked"><p v-if="clocked">Clock In</p>
+<p v-else>Clock Out</p></button>
+    
   </div>
 </template>
 
@@ -10,16 +13,27 @@
 import {mapActions, mapGetters} from 'vuex'
 
 export default {
+
+  async created() {
+    await this.fetchUserClocks()
+    this.user = this.getCurrentUser
+    console.log(this.user)
+  },
   data() {
     return {
-
+      clocked: true,
+      user: {}
     }
   },
   methods: {
-
+    ...mapActions({
+      fetchUserClocks: 'currentUser/fetchClocks'
+    })
   },
   computed: {
-
+    ...mapGetters({
+      getCurrentUser: 'currentUser/getUser'
+    })
   }
 }
 
