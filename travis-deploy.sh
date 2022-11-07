d5dacc7bae69ad2c3194568214b309049565838d
Splyters <nacim.boussaha@epitech.eu>
2022-11-07 15:29:50 +0100

    modified travis deploy

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -8,21 +8,8 @@ sudo curl -L https://raw.githubusercontent.com/docker/compose-cli/main/scripts/i
 sudo docker-compose -f docker-compose.build.yml build
 sudo docker-compose -f docker-compose.build.yml push
 
-# # Installing AWS CLI
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip > /dev/null
-# sudo ./aws/install
-
-# sudo docker context create ecs deploy --from-env
+# Create ECS environment and deploy services on AWS
+sudo docker context create ecs deploy --from-env
 sudo docker context create ecs --from-env deploy
 sudo docker context use deploy
 sudo docker compose -f docker-compose.prod.yml up
-
-# # Login AWS
-# aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-# aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-# aws configure set region $AWS_DEFAULT_REGION
-
-# Update service
-# aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_WEB --force-new-deployment
-# aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_CLIENT --force-new-deployment
