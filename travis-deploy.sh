4f87a53b00299a4ab0a9a51ffe52b069807de5f0
Splyters <nacim.boussaha@epitech.eu>
2022-11-07 15:41:16 +0100

    curl compose cli after build

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -4,9 +4,10 @@
 docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PASS
 
 # Push Dockerfile to DockerHub
-sudo curl -L https://raw.githubusercontent.com/docker/compose-cli/main/scripts/install/install_linux.sh | sh
+
 sudo docker-compose -f docker-compose.build.yml build
 sudo docker-compose -f docker-compose.build.yml push
+sudo curl -L https://raw.githubusercontent.com/docker/compose-cli/main/scripts/install/install_linux.sh | sh
 
 # Create ECS environment and deploy services on AWS
 sudo docker context create ecs deploy --from-env
