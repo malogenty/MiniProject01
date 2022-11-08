24f65aa474f2c26f4d1b1d9494ac91b3fbfb19e9
itsmehujo <itsmehujo@gmail.com>
2022-11-08 12:41:59 +0100

    add schedule seeds to script

--- server/priv/repo/seeds/schedule_seeds.exs
+++ server/priv/repo/seeds/schedule_seeds.exs
@@ -15,28 +15,28 @@ Repo.insert!(%Schedule{
 })
 
 Repo.insert!(%Schedule{
-  date: ~N[2022-11-03 08:00:00],
+  start: ~N[2022-11-03 08:00:00],
   duration: 4.0,
   title: :work,
   user_id: 3
 })
 
 Repo.insert!(%Schedule{
-  date: ~N[2022-11-03 13:00:00],
+  start: ~N[2022-11-03 13:00:00],
   duration: 4.0,
   title: :work,
   user_id: 3
 })
 
 Repo.insert!(%Schedule{
-  date: ~N[2022-11-04 08:00:00],
+  start: ~N[2022-11-04 08:00:00],
   duration: 4.0,
   title: :work,
   user_id: 3
 })
 
 Repo.insert!(%Schedule{
-  date: ~N[2022-11-04 13:00:00],
+  start: ~N[2022-11-04 13:00:00],
   duration: 4.0,
   title: :work,
   user_id: 3
