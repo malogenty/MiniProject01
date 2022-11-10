4646fb3dab15992ce073c59fdf3ea8acd8975a76
Enzo Rojo <enzo.rojo-allendes@epitech.eu>
2022-11-10 16:22:22 +0100

    update calendar style

--- client/src/components/v2/UserDashboard/ScheduleTab.vue
+++ client/src/components/v2/UserDashboard/ScheduleTab.vue
@@ -1,23 +1,23 @@
 <template>
   <ContainerLayout>
     <div class="user-schedule">
-      <span class="header">{{givenUser.username}}'s schedule</span>
+      <span class="header">{{ givenUser.username }}'s schedule</span>
       <div class="calendar-container">
-        <VueCal 
-        ref="vuecal"
-        hideWeekends
-        hideViewSelector 
-        :dblclickToNavigate="false" 
-        :editable-events="canEdit"
-        :drag-to-create-event="false"
-        @cell-dblclick="canEdit ? createBasicEvent($event) : null"
-        @event-drop="updateEvent"
-        @event-duration-change="updateEvent"
-        @event-delete="deleteEvent"
-        @view-change="editView"
-        @ready="setupCal"
+        <VueCal
+          ref="vuecal"
+          hideWeekends
+          hideViewSelector
+          :dblclickToNavigate="false"
+          :editable-events="canEdit"
+          :drag-to-create-event="false"
+          @cell-dblclick="canEdit ? createBasicEvent($event) : null"
+          @event-drop="updateEvent"
+          @event-duration-change="updateEvent"
+          @event-delete="deleteEvent"
+          @view-change="editView"
+          @ready="setupCal"
         >
-      </VueCal>
+        </VueCal>
       </div>
     </div>
   </ContainerLayout>
@@ -33,14 +33,22 @@ import moment from 'moment'
 
 export default {
   async mounted() {
-    await this.fetchSchedule({u_id: this.givenUser.id, from: moment().subtract(2, "week").format("YYYY-MM-DD"), to: moment().add(2, "week").format("YYYY-MM-DD")})
-    Object.values(this.watchedUser.schedule).forEach(ev => {
-      const calEv = this.$refs.vuecal.createEvent(new Date(ev.start), ev.duration * 60, {title: ev.title})
+    await this.fetchSchedule({
+      u_id: this.givenUser.id,
+      from: moment().subtract(2, 'week').format('YYYY-MM-DD'),
+      to: moment().add(2, 'week').format('YYYY-MM-DD')
+    })
+    Object.values(this.watchedUser.schedule).forEach((ev) => {
+      const calEv = this.$refs.vuecal.createEvent(
+        new Date(ev.start),
+        ev.duration * 60,
+        { title: ev.title }
+      )
       this.eidToId[calEv._eid] = ev.id
     })
   },
   props: {
-      givenUser: {},
+    givenUser: {}
   },
   data() {
     return {
@@ -52,49 +60,67 @@ export default {
   components: { ContainerLayout, VueCal },
   computed: {
     ...mapGetters({
-      watchedUser: "watchedUser/getUser",
-      currentUser: "currentUser/getUser"
+      watchedUser: 'watchedUser/getUser',
+      currentUser: 'currentUser/getUser'
     }),
     canEdit() {
-      return (this.currentUser.role === "manager" && this.watchedUser.role === "employee") || (this.currentUser.role === "general_manager" && (this.watchedUser.role === "employee" || this.watchedUser.role === "manager"))
+      return (
+        (this.currentUser.role === 'manager' &&
+          this.watchedUser.role === 'employee') ||
+        (this.currentUser.role === 'general_manager' &&
+          (this.watchedUser.role === 'employee' ||
+            this.watchedUser.role === 'manager'))
+      )
     }
   },
   methods: {
     ...mapActions({
       createScheduleEvent: 'watchedUser/createScheduleEvent',
-      fetchSchedule: "watchedUser/fetchSchedule",
+      fetchSchedule: 'watchedUser/fetchSchedule',
       updateScheduleEvent: 'watchedUser/updateScheduleEvent',
       deleteScheduleEvent: 'watchedUser/deleteScheduleEvent'
     }),
-    async createBasicEvent (ev) {
-      const event = this.$refs.vuecal.createEvent( ev, 240, { title: 'Work'})
+    async createBasicEvent(ev) {
+      const event = this.$refs.vuecal.createEvent(ev, 240, { title: 'Work' })
       return this.createEventDB(event)
     },
-    async setupCal({startDate, endDate}) {
+    async setupCal({ startDate, endDate }) {
       this.startDate = startDate
       this.endDate = endDate
     },
-    async editView({startDate, endDate}) {
-      if(startDate === this.startDate) {
+    async editView({ startDate, endDate }) {
+      if (startDate === this.startDate) {
         this.endDate = endDate
       } else if (endDate === this.endDate) {
         this.startDate = startDate
       }
-      await this.fetchSchedule({u_id: this.givenUser.id, from: moment(this.startDate).subtract(1, "week").format("YYYY-MM-DD"), to: moment(this.endDate).add(1, "week").format("YYYY-MM-DD")})
+      await this.fetchSchedule({
+        u_id: this.givenUser.id,
+        from: moment(this.startDate).subtract(1, 'week').format('YYYY-MM-DD'),
+        to: moment(this.endDate).add(1, 'week').format('YYYY-MM-DD')
+      })
     },
     async createEventDB(event) {
       this.roundToHalf(event.start)
       this.roundToHalf(event.end)
       console.log(event.start)
       const duration = moment(event.end).diff(event.start) / 1000 / 60
-      await this.createScheduleEvent({u_id: this.givenUser.id, duration, ...event})
+      await this.createScheduleEvent({
+        u_id: this.givenUser.id,
+        duration,
+        ...event
+      })
       return event
     },
-    async updateEvent({event}) {
+    async updateEvent({ event }) {
       this.roundToHalf(event.start)
       this.roundToHalf(event.end)
       const duration = moment(event.end).diff(event.start) / 1000 / 60
-      await this.updateScheduleEvent({ev_id: this.eidToId[event._eid], duration, ...event})
+      await this.updateScheduleEvent({
+        ev_id: this.eidToId[event._eid],
+        duration,
+        ...event
+      })
     },
     async deleteEvent(event) {
       await this.deleteScheduleEvent(this.eidToId[event._eid])
@@ -102,12 +128,12 @@ export default {
     roundToHalf(date) {
       let m = date.getMinutes()
       date.setSeconds(0)
-      if(m < 15) {
+      if (m < 15) {
         date.setMinutes(0)
       } else if (m > 30) {
         date.setMinutes(0)
         date.setHours(date.getHours() + 1)
-      } else if (m > 15){
+      } else if (m > 15) {
         date.setMinutes(30)
       }
       return date
@@ -117,23 +143,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "@/styles/colors.scss";
-  .container {
-    background-color: $khaki;
-  border-radius: 12px;
-
-    .user-schedule {
-      min-height: 500px;
-      display: flex;
-      flex-direction: column;
-      .header {
-        text-align: center;
-        font-weight: bold;
-        font-size: 1.2em;
-      }
-      .calendar-container {
-        height: 600px;
-      }
+@import '@/styles/colors.scss';
+.container {
+  .user-schedule {
+    background-color: $light_grey;
+    padding: 2%;
+    border-radius: 10px;
+    min-height: 500px;
+    display: flex;
+    flex-direction: column;
+    .header {
+      font-weight: bold;
+      font-size: 1.5em;
+      margin: 0 auto 0 0;
+    }
+    .calendar-container {
+      height: 600px;
     }
   }
+}
 </style>
