ad64eccba219116c8e094ec41715964358dfbf43
Enzo Rojo <enzo.rojo-allendes@epitech.eu>
2022-11-10 17:43:41 +0100

    update settings

--- client/src/components/v2/UserDashboard/SettingsTab.vue
+++ client/src/components/v2/UserDashboard/SettingsTab.vue
@@ -1,21 +1,37 @@
 <template>
   <ContainerLayout>
     <div class="settings">
-      <span class="header">{{user.username}}'s settings</span>
-      <form @submit.prevent="handleFormSubmission">
-        <label>
-          username
-          <input type="text" v-model="user.username" :disabled="disabled" @input="message = {}"/>
-        </label>
-        <label>
-          email
-          <input type="email" v-model="user.email" :disabled="disabled" @input="message = {}"/>
-        </label>
+      <span class="header">{{ user.username }}'s settings</span>
+      <form class="form" @submit.prevent="handleFormSubmission">
+        <div class="form-group">
+          <label> username </label>
+          <input
+            type="text"
+            v-model="user.username"
+            :disabled="disabled"
+            @input="message = {}"
+          />
+        </div>
+        <div class="form-group">
+          <label> email </label>
+          <input
+            type="email"
+            v-model="user.email"
+            :disabled="disabled"
+            @input="message = {}"
+          />
+        </div>
         <button type="submit" :disabled="disabled">Submit</button>
-        <span class="message" :class="{valid: message.status === 200, visible: message.msg}">{{message.msg}}</span>
+        <span
+          class="message"
+          :class="{ valid: message.status === 200, visible: message.msg }"
+          >{{ message.msg }}</span
+        >
       </form>
       <div class="actions">
-        <button @click="promoteThisUser" v-if="canBePromoted">Promote user</button>
+        <button @click="promoteThisUser" v-if="canBePromoted">
+          Promote user
+        </button>
         <button @click="deleteThisUser" v-if="canBeDeleted">Delete user</button>
       </div>
     </div>
@@ -26,7 +42,7 @@
 import { mapActions, mapGetters } from 'vuex'
 import ContainerLayout from '../../Layout/ContainerLayout.vue'
 export default {
-  components: {ContainerLayout},
+  components: { ContainerLayout },
   props: {
     givenUser: {}
   },
@@ -45,21 +61,32 @@ export default {
       currentUser: 'currentUser/getUser'
     }),
     canBePromoted() {
-      return this.currentUser.role === "general_manager" && this.user.role !== "manager" && this.user.role !== "general_manager"
+      return (
+        this.currentUser.role === 'general_manager' &&
+        this.user.role !== 'manager' &&
+        this.user.role !== 'general_manager'
+      )
     },
     canBeDeleted() {
-      return this.currentUser.id === this.user.id || (this.currentUser.role === "general_manager" && this.user.role !== "general_manager")
+      return (
+        this.currentUser.id === this.user.id ||
+        (this.currentUser.role === 'general_manager' &&
+          this.user.role !== 'general_manager')
+      )
     }
   },
   methods: {
     ...mapActions({
       editUser: 'currentUser/editUser',
       promoteUser: 'watchedUser/promoteUser',
-      deleteUser: 'watchedUser/deleteUser',
+      deleteUser: 'watchedUser/deleteUser'
     }),
     async handleFormSubmission() {
-      if(this.disabled) return
-      const res = await this.editUser({email: this.user.email, username: this.user.username})
+      if (this.disabled) return
+      const res = await this.editUser({
+        email: this.user.email,
+        username: this.user.username
+      })
       this.editMessage(res)
     },
     async promoteThisUser() {
@@ -71,33 +98,53 @@ export default {
       this.editMessage(res)
     },
     editMessage(res) {
-      if (res.status === 200) this.message = {status: res.status, msg: "Success !"}
-      if (res.status !== 200) this.message = {status: res.status, msg: res.error}
+      if (res.status === 200)
+        this.message = { status: res.status, msg: 'Success !' }
+      if (res.status !== 200)
+        this.message = { status: res.status, msg: res.error }
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-@import "@/styles/colors.scss";
+@import '@/styles/colors.scss';
 .container {
-  background: $gray;
-  border-radius: 12px;
   .settings {
+    background: $light_grey;
+    padding: 2%;
+    border-radius: 10px;
     display: flex;
     flex-direction: column;
     min-height: 500px;
     .header {
-      text-align: center;
       font-weight: bold;
-      font-size: 1.2em;
+      font-size: 1.5em;
+      margin: 0 auto 0 0;
     }
-    form {
+    .form {
       margin-top: 24px;
       display: flex;
       flex-direction: column;
-      align-items: center;
+      align-items: flex-start;
+      max-width: 30%;
       gap: 8px;
+      &-group {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        input {
+          border-radius: 6px;
+          padding: 1%;
+          border: 1px solid rgb(219, 219, 219);
+        }
+
+        input:focus {
+          outline: solid;
+          outline-width: 1px;
+          outline-color: black;
+        }
+      }
       label {
         width: min-content;
       }
@@ -113,6 +160,19 @@ export default {
         &.visible {
           display: inline;
         }
+      }
+    }
+    button {
+      height: 35px;
+      border-radius: 6px;
+      transition: all 200ms ease;
+      border: none;
+      cursor: pointer;
+      background-color: white;
+      border: 1px solid rgb(219, 219, 219);
+      &:hover {
+        box-shadow: 2px 2px 0px 0px darken(#f0f0f0, 20%);
+        -webkit-box-shadow: 2px 2px 0px 0px darken(#f0f0f0, 20%);
       }
     }
     .actions {
